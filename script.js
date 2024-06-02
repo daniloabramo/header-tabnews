b37a532d284b32ddd0469ed48cbbef8ea1e88465
daniloabramo <142537432+daniloabramo@users.noreply.github.com>
2024-06-02 18:23:31 -0300

    adjustments to coin and crystal icons

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function ppCrystal() {
     var imagem2 = document.getElementById("tabcash");
     
     var novoCaminho1 = "/icons/p-coin.svg"; 
-    var novoCaminho2 = "/icons/p-crystal.svg"; 
+    var novoCaminho2 = "/icons/icons-v2/crystal-pp-50.svg"; 
     
     imagem1.src = novoCaminho1;
     imagem2.src = novoCaminho2;
@@ -33,8 +33,8 @@ function mmCrystal() {
     var imagem1 = document.getElementById("tabcoin");
     var imagem2 = document.getElementById("tabcash");
     
-    var novoCaminho1 = "/icons/m-coin.svg"; 
-    var novoCaminho2 = "/icons/m-crystal.svg"; 
+    var novoCaminho1 = "/icons/icons-v2/coin-mm-50.svg"; 
+    var novoCaminho2 = "/icons/icons-v2/crystal-mm-50.svg"; 
     
     imagem1.src = novoCaminho1;
     imagem2.src = novoCaminho2;
@@ -44,8 +44,8 @@ function mgCrystal() {
     var imagem1 = document.getElementById("tabcoin");
     var imagem2 = document.getElementById("tabcash");
     
-    var novoCaminho1 = "/icons/mg-coin.svg"; 
-    var novoCaminho2 = "/icons/mg-crystal.svg"; 
+    var novoCaminho1 = "/icons/icons-v2/coin-mg-50.svg"; 
+    var novoCaminho2 = "/icons/icons-v2/crystal-mg-50.svg"; 
     
     imagem1.src = novoCaminho1;
     imagem2.src = novoCaminho2;
@@ -55,8 +55,8 @@ function ggCrystal() {
     var imagem1 = document.getElementById("tabcoin");
     var imagem2 = document.getElementById("tabcash");
     
-    var novoCaminho1 = "/icons/g-coin.svg"; 
-    var novoCaminho2 = "/icons/g-crystal.svg"; 
+    var novoCaminho1 = "/icons/icons-v2/coin-gg-50.svg"; 
+    var novoCaminho2 = "/icons/icons-v2/crystal-gg-50.svg"; 
     
     imagem1.src = novoCaminho1;
     imagem2.src = novoCaminho2;
@@ -88,7 +88,7 @@ function mmCube() {
     var imagem1 = document.getElementById("tabcoin");
     var imagem2 = document.getElementById("tabcash");
     
-    var novoCaminho1 = "/icons/m-coin.svg"; 
+    var novoCaminho1 = "/icons/icons-v2/coin-mm-50.svg"; 
     var novoCaminho2 = "/icons/m-cube.svg"; 
     
     imagem1.src = novoCaminho1;
@@ -99,7 +99,7 @@ function mgCube() {
     var imagem1 = document.getElementById("tabcoin");
     var imagem2 = document.getElementById("tabcash");
     
-    var novoCaminho1 = "/icons/mg-coin.svg"; 
+    var novoCaminho1 = "/icons/icons-v2/coin-mg-50.svg"; 
     var novoCaminho2 = "/icons/mg-cube.svg"; 
     
     imagem1.src = novoCaminho1;
@@ -110,7 +110,7 @@ function ggCube() {
     var imagem1 = document.getElementById("tabcoin");
     var imagem2 = document.getElementById("tabcash");
     
-    var novoCaminho1 = "/icons/g-coin.svg"; 
+    var novoCaminho1 = "/icons/icons-v2/coin-gg-50.svg"; 
     var novoCaminho2 = "/icons/g-cube.svg"; 
     
     imagem1.src = novoCaminho1;
