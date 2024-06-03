1bcd43e7b65f2aeddfd641dc2f0fda86a9a076a7
daniloabramo <142537432+daniloabramo@users.noreply.github.com>
2024-06-03 13:38:32 -0300

    Full version icons; New scales; Interaction to select scale; Small general adjustments

--- script.js
+++ script.js
@@ -7,114 +7,147 @@ document.querySelectorAll('.btn').forEach(btn => {
     });
 });  
 
+document.querySelectorAll('.btn2').forEach(btn => {
+    btn.addEventListener('click', function() {
+
+      document.querySelectorAll('.btn2').forEach(b => b.classList.remove('background2'));
+      
+      this.classList.add('background2');
+    });
+});  
+
 function original() {
-    var imagem1 = document.getElementById("tabcoin");
-    var imagem2 = document.getElementById("tabcash");
+    var img1 = document.getElementById("tabcoin");
+    var img2 = document.getElementById("tabcash");
     
-    var novoCaminho1 = "/icons/original-coin.svg"; 
-    var novoCaminho2 = "/icons/original-cash.svg"; 
+    var newRoute1 = "/icons/original-coin.svg"; 
+    var newRoute2 = "/icons/original-cash.svg"; 
     
-    imagem1.src = novoCaminho1;
-    imagem2.src = novoCaminho2;
+    img1.src = newRoute1;
+    img2.src = newRoute2;
 }
 
 function ppCrystal() {
-    var imagem1 = document.getElementById("tabcoin");
-    var imagem2 = document.getElementById("tabcash");
+    var img1 = document.getElementById("tabcoin");
+    var img2 = document.getElementById("tabcash");
     
-    var novoCaminho1 = "/icons/p-coin.svg"; 
-    var novoCaminho2 = "/icons/icons-v2/crystal-pp-50.svg"; 
+    var newRoute1 = "/icons/icons-v2/coin-full-2.svg"; 
+    var newRoute2 = "/icons/icons-v2/crystal-full-2.svg"; 
     
-    imagem1.src = novoCaminho1;
-    imagem2.src = novoCaminho2;
+    img1.src = newRoute1;
+    img2.src = newRoute2;
 }
 
 function mmCrystal() {
-    var imagem1 = document.getElementById("tabcoin");
-    var imagem2 = document.getElementById("tabcash");
+    var img1 = document.getElementById("tabcoin");
+    var img2 = document.getElementById("tabcash");
     
-    var novoCaminho1 = "/icons/icons-v2/coin-mm-50.svg"; 
-    var novoCaminho2 = "/icons/icons-v2/crystal-mm-50.svg"; 
+    var newRoute1 = "/icons/icons-v2/coin-mm-50.svg"; 
+    var newRoute2 = "/icons/icons-v2/crystal-mm-50.svg"; 
     
-    imagem1.src = novoCaminho1;
-    imagem2.src = novoCaminho2;
+    img1.src = newRoute1;
+    img2.src = newRoute2;
 }
 
 function mgCrystal() {
-    var imagem1 = document.getElementById("tabcoin");
-    var imagem2 = document.getElementById("tabcash");
+    var img1 = document.getElementById("tabcoin");
+    var img2 = document.getElementById("tabcash");
     
-    var novoCaminho1 = "/icons/icons-v2/coin-mg-50.svg"; 
-    var novoCaminho2 = "/icons/icons-v2/crystal-mg-50.svg"; 
+    var newRoute1 = "/icons/icons-v2/coin-mg-50.svg"; 
+    var newRoute2 = "/icons/icons-v2/crystal-mg-50.svg"; 
     
-    imagem1.src = novoCaminho1;
-    imagem2.src = novoCaminho2;
+    img1.src = newRoute1;
+    img2.src = newRoute2;
 }
 
 function ggCrystal() {
-    var imagem1 = document.getElementById("tabcoin");
-    var imagem2 = document.getElementById("tabcash");
+    var img1 = document.getElementById("tabcoin");
+    var img2 = document.getElementById("tabcash");
     
-    var novoCaminho1 = "/icons/icons-v2/coin-gg-50.svg"; 
-    var novoCaminho2 = "/icons/icons-v2/crystal-gg-50.svg"; 
+    var newRoute1 = "/icons/icons-v2/coin-gg-50.svg"; 
+    var newRoute2 = "/icons/icons-v2/crystal-gg-50.svg"; 
     
-    imagem1.src = novoCaminho1;
-    imagem2.src = novoCaminho2;
+    img1.src = newRoute1;
+    img2.src = newRoute2;
 }
 
 function original2() {
-    var imagem1 = document.getElementById("tabcoin");
-    var imagem2 = document.getElementById("tabcash");
+    var img1 = document.getElementById("tabcoin");
+    var img2 = document.getElementById("tabcash");
     
-    var novoCaminho1 = "/icons/original-coin.svg"; 
-    var novoCaminho2 = "/icons/original-cash.svg"; 
+    var newRoute1 = "/icons/original-coin.svg"; 
+    var newRoute2 = "/icons/original-cash.svg"; 
     
-    imagem1.src = novoCaminho1;
-    imagem2.src = novoCaminho2;
+    img1.src = newRoute1;
+    img2.src = newRoute2;
 }
 
 function ppCube() {
-    var imagem1 = document.getElementById("tabcoin");
-    var imagem2 = document.getElementById("tabcash");
+    var img1 = document.getElementById("tabcoin");
+    var img2 = document.getElementById("tabcash");
     
-    var novoCaminho1 = "/icons/p-coin.svg"; 
-    var novoCaminho2 = "/icons/p-cube.svg"; 
+    var newRoute1 = "/icons/icons-v2/coin-full-2.svg"; 
+    var newRoute2 = "/icons/icons-v2/cube.svg"; 
     
-    imagem1.src = novoCaminho1;
-    imagem2.src = novoCaminho2;
+    img1.src = newRoute1;
+    img2.src = newRoute2;
 }
 
 function mmCube() {
-    var imagem1 = document.getElementById("tabcoin");
-    var imagem2 = document.getElementById("tabcash");
+    var img1 = document.getElementById("tabcoin");
+    var img2 = document.getElementById("tabcash");
     
-    var novoCaminho1 = "/icons/icons-v2/coin-mm-50.svg"; 
-    var novoCaminho2 = "/icons/m-cube.svg"; 
+    var newRoute1 = "/icons/icons-v2/coin-mm-50.svg"; 
+    var newRoute2 = "/icons/m-cube.svg"; 
     
-    imagem1.src = novoCaminho1;
-    imagem2.src = novoCaminho2;
+    img1.src = newRoute1;
+    img2.src = newRoute2;
 }
 
 function mgCube() {
-    var imagem1 = document.getElementById("tabcoin");
-    var imagem2 = document.getElementById("tabcash");
+    var img1 = document.getElementById("tabcoin");
+    var img2 = document.getElementById("tabcash");
     
-    var novoCaminho1 = "/icons/icons-v2/coin-mg-50.svg"; 
-    var novoCaminho2 = "/icons/mg-cube.svg"; 
+    var newRoute1 = "/icons/icons-v2/coin-mg-50.svg"; 
+    var newRoute2 = "/icons/mg-cube.svg"; 
     
-    imagem1.src = novoCaminho1;
-    imagem2.src = novoCaminho2;
+    img1.src = newRoute1;
+    img2.src = newRoute2;
 }
 
 function ggCube() {
-    var imagem1 = document.getElementById("tabcoin");
-    var imagem2 = document.getElementById("tabcash");
+    var img1 = document.getElementById("tabcoin");
+    var img2 = document.getElementById("tabcash");
     
-    var novoCaminho1 = "/icons/icons-v2/coin-gg-50.svg"; 
-    var novoCaminho2 = "/icons/g-cube.svg"; 
+    var newRoute1 = "/icons/icons-v2/coin-gg-50.svg"; 
+    var newRoute2 = "/icons/g-cube.svg"; 
     
-    imagem1.src = novoCaminho1;
-    imagem2.src = novoCaminho2;
+    img1.src = newRoute1;
+    img2.src = newRoute2;
+}
+
+function ppSize() {
+    var icones = document.querySelectorAll('.icon');
+
+    icones.forEach(icone => {
+        icone.style.width = '0.75rem';
+    });
+}
+
+function mmSize() {
+    var icones = document.querySelectorAll('.icon');
+
+    icones.forEach(icone => {
+        icone.style.width = '0.875rem';
+    });
+}
+
+function ggSize() {
+    var icones = document.querySelectorAll('.icon');
+
+    icones.forEach(icone => {
+        icone.style.width = '1rem';
+    });
 }
 
 document.getElementById("original").addEventListener("click", original);
@@ -127,4 +160,8 @@ document.getElementById("pp2").addEventListener("click", ppCube);
 document.getElementById("mm2").addEventListener("click", mmCube);
 document.getElementById("mg2").addEventListener("click", mgCube);
 document.getElementById("gg2").addEventListener("click", ggCube);
+document.getElementById('twelve').addEventListener('click', ppSize);
+document.getElementById('fourteen').addEventListener('click', mmSize);
+document.getElementById('sixteen').addEventListener('click', ggSize);
+
 
